1fe4a08d7dc1b65685b5819e2745c08d694d2418
Gustavo Raposo <raposo@pop-os.localdomain>
2021-05-17 23:17:42 -0300

    Fixed Experience text

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -11,13 +11,14 @@ function Experience() {
 					<h2>Lactec - Estagiário Técnico</h2>
 					<small>(2020 - 2021)</small>
 					<p>
-						Fui encarregado de desenvolver um aplicativo por completo através da plataforma Android Studio
-						inicialmente desenvolvido em Java. A aplicação consistia em se comunicar com uma placa
-						utilizando Bluetooth Low Energy para transmitir e recuperar dados da placa e apresentando
-						estes dados no dispositivo.<br />
-						Após o mvp concretizado desta aplicação, tomei liberdade em propor a abordagem do projeto
-						para uma arquitetura mais concreta, no caso foi sugerido MVVM, além da transcrição do código
-						para Kotlin.<br />
+						Desenvolvi um aplicativo através da plataforma Android Studio inicialmente desenvolvido em Java.
+						A aplicação consistia em se comunicar com uma placa utilizando Bluetooth Low Energy para
+						transmitir e recuperar dados da placa e apresentando estes dados no dispositivo.<br />
+						Após o mvp concretizado desta aplicação, tomei liberdade em propor alterações no projeto em
+						relação a arquiterua e utilização de padrões de projetos, no caso foi sugerido MVVM,
+						além da transcrição do código para Kotlin, recriação dos layouts seguindo as guidelines
+						do material design e a utilização de ConstraintLayout para tornar a interface mais
+						responsiva.<br />
 					</p>
 				</li>
 			</ul>
